05b6680d9e476d9d2683776e7e31902c7ab8439c
Vimato <monst3k003@gmail.com>
2025-12-25 16:44:49 +0700

    Delete unused imports

--- lab1_1/app/src/main/java/com/example/lab1/BaseActivity.kt
+++ lab1_1/app/src/main/java/com/example/lab1/BaseActivity.kt
@@ -1,12 +1,10 @@
 package com.example.lab1
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
@@ -18,7 +16,6 @@ abstract class BaseActivity : AppCompatActivity()  {
     protected abstract val buttonId: Int
     protected abstract val nextActivityClass: Class<*>
 
-    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- lab1_2/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1_2/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.lab1
 
-import android.app.Activity
-import android.app.Application
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
